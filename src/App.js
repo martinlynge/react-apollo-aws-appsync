41e80fee56ebe2fe01e5b70c4862c32d7e792312
martinlynge <tslynge@gmail.com>
2019-04-01 23:57:25 +0200

    Added simple app bar

--- src/App.js
+++ src/App.js
@@ -3,10 +3,20 @@ import Client from './Client';
 import { Router } from '@reach/router';
 import ListEvents from './components/ListEvents';
 import ViewEvent from './components/ViewEvent';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
 
 export default function App() {
   return (
     <Client>
+      <AppBar position="static" color="primary">
+        <Toolbar variant="dense">
+          <Typography variant="h6" color="inherit">
+            Events
+          </Typography>
+        </Toolbar>
+      </AppBar>
       <Router>
         <ListEvents path="/" />
         <ViewEvent path=":eventId" />
