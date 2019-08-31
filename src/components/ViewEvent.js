1a1849b11093fd448d5f944ba77bc79c7ebcf7ca
Martin Lynge Johannsen <martinlynge@users.noreply.github.com>
2019-08-31 17:52:07 +0200

    Migrate to hooks (#1)

--- src/components/ViewEvent.js
+++ src/components/ViewEvent.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Query } from 'react-apollo';
+import { useQuery } from '@apollo/react-hooks';
 import gql from 'graphql-tag';
 import UpdateEvent from './UpdateEvent';
 import Loading from './Loading';
@@ -43,45 +43,41 @@ const styles = () => ({
 });
 
 function ViewEvent({ eventId, classes }) {
+  const { loading, error, data } = useQuery(GET_EVENT, {
+    variables: { id: eventId },
+  });
+
+  if (loading) return <Loading />;
+  if (error) return <p>Error :(</p>;
+
+  const { name, when, where, description } = data.getEvent;
+
   return (
-    <Query query={GET_EVENT} variables={{ id: eventId }}>
-      {({ loading, error, data }) => {
-        if (loading) return <Loading />;
-        if (error) return <p>Error :(</p>;
-        const { name, when, where, description } = data.getEvent;
-        return (
-          <div className={classes.wrapper}>
-            <Container maxWidth="md">
-              <Card elevation={0}>
-                <CardHeader
-                  title={name}
-                  subheader={dayjs(when).format('ddd, MMM D, YYYY HH:mm')}
-                />
-                <CardMedia className={classes.media} image={cover(name)} />
-                <CardContent>
-                  <Grid
-                    container
-                    alignItems="center"
-                    style={{ marginBottom: 16 }}
-                  >
-                    <PlaceIcon
-                      color="action"
-                      fontSize="small"
-                      style={{ marginLeft: -4, marginRight: 4 }}
-                    />
-                    <Typography variant="body2">{where}</Typography>
-                  </Grid>
-                  <Typography variant="body1">{description}</Typography>
-                </CardContent>
-              </Card>
-              <div className={classes.form}>
-                <UpdateEvent data={data.getEvent} />
-              </div>
-            </Container>
-          </div>
-        );
-      }}
-    </Query>
+    <div className={classes.wrapper}>
+      <Container maxWidth="md">
+        <Card elevation={0}>
+          <CardHeader
+            title={name}
+            subheader={dayjs(when).format('ddd, MMM D, YYYY HH:mm')}
+          />
+          <CardMedia className={classes.media} image={cover(name)} />
+          <CardContent>
+            <Grid container alignItems="center" style={{ marginBottom: 16 }}>
+              <PlaceIcon
+                color="action"
+                fontSize="small"
+                style={{ marginLeft: -4, marginRight: 4 }}
+              />
+              <Typography variant="body2">{where}</Typography>
+            </Grid>
+            <Typography variant="body1">{description}</Typography>
+          </CardContent>
+        </Card>
+        <div className={classes.form}>
+          <UpdateEvent data={data.getEvent} />
+        </div>
+      </Container>
+    </div>
   );
 }
 
